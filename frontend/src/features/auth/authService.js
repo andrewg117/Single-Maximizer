ff4581664ca61f948b3124f7e8cbff288fcc4d01
andrewg117 <andrewjgrant7@gmail.com>
2023-09-10 14:56:13 -0400

    Testing API

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -35,6 +35,8 @@ const emailData = async (token) => {
 
 const login = async (userData) => {
   const response = await axios.post(API_URL + 'login', userData)
+  console.log(NODE_ENV)
+  console.log(RENDER_API_URL)
   console.log(API_URL)
 
   if (response.data) {
