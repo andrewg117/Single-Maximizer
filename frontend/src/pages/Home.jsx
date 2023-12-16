6b1cf8ec5d3ebef5d94fd90a3201aa4a2c41046f
andrewg117 <andrewjgrant7@gmail.com>
2023-12-16 14:04:19 -0500

    Updating css and moving FAQ page to Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import homeImage from "../images/homeImage.png";
-import styles from "../css/style.module.css";
+import homeImage2 from "../images/homeImage2.png";
+import homeImage3 from "../images/homeImage3.png";
+import singlemax from "../images/singlemax.png";
+import styles from "../css/home.module.css";
+import { FAQDistro, FAQSave } from "./FAQ";
 
 function Home() {
   return (
     <>
       <section
-        id={styles.home_block}
+        className={styles.home_block}
         style={{ backgroundImage: `url(${homeImage})` }}
       >
         <div id={styles.intro_text}>
@@ -16,6 +20,44 @@ function Home() {
           <Link to="/home/emailsignup">GET STARTED</Link>
         </div>
       </section>
+
+      {/* <section
+        id="singleMax-image"
+        className={styles.home_block}
+        style={{ backgroundImage: `url(${singlemax})` }}
+      ></section>
+
+      <section className={styles.home_block}>
+        <FAQSave />
+      </section>
+
+      <section
+        className={styles.home_block}
+        style={{ backgroundImage: `url(${homeImage2})` }}
+      >
+        <div id={styles.intro_text}>
+          <h1>GET YOUR MUSIC OUT TO THE WORLD</h1>
+        </div>
+        <div id={styles.intro_btn}>
+          <Link to="/home/emailsignup">GET STARTED</Link>
+        </div>
+      </section>
+
+      <section className={styles.home_block}>
+        <FAQDistro />
+      </section>
+      
+      <section
+        className={styles.home_block}
+        style={{ backgroundImage: `url(${homeImage3})` }}
+      >
+        <div id={styles.intro_text}>
+          <h1>GET YOUR MUSIC OUT TO THE WORLD</h1>
+        </div>
+        <div id={styles.intro_btn}>
+          <Link to="/home/emailsignup">GET STARTED</Link>
+        </div>
+      </section> */}
     </>
   );
 }
